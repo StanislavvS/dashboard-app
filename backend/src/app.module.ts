4b8993d8bbd0684ad74302fd69efe4ac4403ba0d
Stanislaw Sobczak <sstanislaw85@gmail.com>
2022-11-15 15:24:03 +0100

    Refector register Controller to auth Controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,19 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RegisterModule } from './register/register.module';
+import { AuthController } from './auth/auth.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    RegisterModule,
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PWD}@cluster0.fwxripz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority/`,
     ),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- backend/src/register/register.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { RegisterService } from './register.service';
-
-@Controller('register')
-export class RegisterController {
-  constructor(private registerService: RegisterService) {}
-}
